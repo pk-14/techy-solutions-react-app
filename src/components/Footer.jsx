38120db44afc83895f27d1b2ae5b6d9e0f713b9c
pk-14 <100840901+pk-14@users.noreply.github.com>
2022-12-20 16:43:17 +0530

    Final Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Techy Solutions</h1>
+            <p>@All Rights Reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://www.linkedin.com/in/pavan-kumar-a466231a6/" target={"blank"}>LinkedIn</a>
+                <a href="https://github.com/pk-14" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
